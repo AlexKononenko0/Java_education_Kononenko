6d95d0eda087235422489a682756e4839dc93215
Alex <o.o.kononenko@student.khai.edu>
2021-10-30 23:07:58 +0300

    Hangman 2-nd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,12 +1,28 @@
 package Hangman;
 
 import java.util.Locale;
+import java.util.Scanner;
+
 
 public class hangman {
     public static void main(String[] args) {
         String str = "HANGMAN";
         String str2 ="The game will be available soon";
         String Up = str.toUpperCase(Locale.ROOT);
-        System.out.println(str + "\n" + str2);
-}
+        System.out.println(str + "\n"+str2);
+        Scanner scanner = new Scanner(System.in);
+        String response;
+        label:
+        while (true){
+            response = scanner.nextLine();
+            switch (response) {
+                case "python":
+                    System.out.println("You lost!");
+                    break label;
+                case "java":
+                    System.out.println("You survived!");
+                    break label;
+            }
+        }
+    }
 }
