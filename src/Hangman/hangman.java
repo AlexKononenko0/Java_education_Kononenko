e7bbf1d8a1f2fc95174cb68dc4700aa3aee40193
Alex <o.o.kononenko@student.khai.edu>
2021-10-31 00:25:32 +0300

    Hangman 3-rd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,8 +1,10 @@
 package Hangman;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
-
+import java.util.Random;
 
 public class hangman {
     public static void main(String[] args) {
@@ -10,20 +12,39 @@ public class hangman {
         String str2 ="The game will be available soon";
         String Up = str.toUpperCase(Locale.ROOT);
         System.out.println(str + "\n"+str2);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("python");
+        list.add("java");
+        list.add("javascript");
+        list.add("kotlin");
+        list.add("php");
+
+        Random random = new Random();
+        int randomitem = random.nextInt(list.size());
+        String randomElement = list.get(randomitem);
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Guess the word:");
         String response;
         label:
         while (true){
             response = scanner.nextLine();
             switch (response) {
-                case "python":
-                    System.out.println("You lost!");
-                    break label;
-                case "java":
-                    System.out.println("You survived!");
+                case ("python"):
+                case ("java"):
+                case ("javascript"):
+                case ("kotlin"):
+                case ("php"):
+                    if(response.length() < randomElement.length())
+                        System.out.println("You lost!");
+                    if(response.length() > randomElement.length())
+                        System.out.println("You lost!");
+                    if (response.length() == randomElement.length())
+                            System.out.println("You survived!");
                     break label;
             }
+
         }
     }
 }
