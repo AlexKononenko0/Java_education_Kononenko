710fb5cd5ae998629ea9dfceb68c85483bfe7336
Alex <o.o.kononenko@student.khai.edu>
2021-10-30 14:19:23 +0300

    Hangman 1-st stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,12 @@
+package Hangman;
+
+import java.util.Locale;
+
+public class hangman {
+    public static void main(String[] args) {
+        String str = "HANGMAN";
+        String str2 ="The game will be available soon";
+        String Up = str.toUpperCase(Locale.ROOT);
+        System.out.println(str + "\n" + str2);
+}
+}
